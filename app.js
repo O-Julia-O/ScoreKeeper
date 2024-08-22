c6e8b6f314310e2b0617f1bd4c359fa24ed1ac32
O-Julia-O <julia.qq.onix@gmail.com>
2024-08-22 11:53:46 +0300

    adding game over

--- app.js
+++ app.js
@@ -6,17 +6,32 @@ const p2ScoreText = document.getElementById("p2ScoreText");
 let p1Score = 0;
 let p2Score = 0;
 let winningScore = 5;
+let isGameOver = false;
 
 p1Button.addEventListener("click", function () {
-  if (p1Score !== winningScore) {
+    /* if the game is not over, than score */
+  if (!isGameOver) {
     p1Score += 1;
+
+    /* if score equals than finish the game */
+    if (p1Score === winningScore) {
+      isGameOver = true;
+    }
+
     p1ScoreText.textContent = p1Score;
   }
 });
 
 p2Button.addEventListener("click", function () {
-  if (p2Score !== winningScore) {
+    /* if the game is not over, than score */
+  if (!isGameOver) {
     p2Score += 1;
+
+    /* if score equals than finish the game */
+    if (p2Score === winningScore) {
+      isGameOver = true;
+    }
+
     p2ScoreText.textContent = p2Score;
   }
 });
